d6a3b624418a4bc9d919bf6d5a88083b13f86615
MelyC2 <melanny.castillo@ister.edu.ec>
2026-08-13 11:13:44 -0500

    Update knexfile.js

--- BACKEND/knexfile.js
+++ BACKEND/knexfile.js
@@ -16,9 +16,6 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      ssl: {
-        rejectUnauthorized: false
-      }
     },
     migrations: {
       directory: "./migrations"
